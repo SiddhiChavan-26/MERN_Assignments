db9b229848e69f33a0d8a046161a13bc8a5ad046
Siddhi Chavan <siddhichavan073@gmail.com>
2025-12-15 23:49:26 +0530

    Assignment03

---
+++ Assignment03/script.js
@@ -0,0 +1,23 @@
+function validatePassword() {
+    let passwordInput = document.getElementById("password")
+    let password = passwordInput.value;
+    let errorMsg = document.getElementById("passwordError");
+
+    if(password.length < 6){
+        errorMsg.innerText = "Password must be at least 6 characters long";
+        passwordInput.classList.add("error");
+        passwordInput.classList.remove("success");
+        return false;
+    }else{
+        errorMsg.innerText = "";
+        passwordInput.classList.remove("error");
+        passwordInput.classList.add("success");
+
+        alert("Login successful!");
+
+        emailInput.value = "";
+        passwordInput.value = "";
+
+        return false;
+    }
+}
